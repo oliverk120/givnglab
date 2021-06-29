337aac57cbf2e2e5dbf2aa3e9130521bb951ffef
oliverk120 <oliverk120@gmail.com>
2021-06-28 20:41:09 -0400

    front angular mockup works at basic level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'givngFrontEnd';
+  title = 'GivngLAB';
+  foods: Food[] = [
+    {value: 'mother', viewValue: 'Mother'},
+    {value: 'brothers', viewValue: 'Brothers'},
+    {value: 'baby daughter', viewValue: 'Baby Daughter'},
+    {value: 'wife', viewValue: 'Wife'}
+  ];
+  panelOpenState = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSelectModule} from '@angular/material/select';
+import { MatExpansionModule} from '@angular/material/expansion'; 
+import { MatCheckboxModule} from '@angular/material/checkbox'; 
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
